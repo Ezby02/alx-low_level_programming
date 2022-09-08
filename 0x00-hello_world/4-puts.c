7bdb93a1ca0593f68ef5bbcc62dfbf486dae97f8
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2022-09-09 00:51:29 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-/**
-*Entry point (main function)
-*Return is 0
-*/
+/*Entry point (main function)*/
 int main(void)
 {
 	/*Use of puts*/
 	puts("\"Programming is like building a multilingual puzzle");
+	/*Return is 0*/
 	return (0);
 }
