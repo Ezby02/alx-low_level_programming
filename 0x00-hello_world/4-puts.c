83abb542373c78a8a2f16c17e79cf18b97edbdba
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2022-09-09 00:28:36 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* Entry point (main function)*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
