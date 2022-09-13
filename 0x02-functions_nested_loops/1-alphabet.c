65ed247c8f73600d5be081be8a83754d1982e62e
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2022-09-13 23:37:49 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,15 +9,15 @@
  */
 int main(void)
 {
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void);
 {
+	print_alphabet();
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
 	_putchar("\n");
+	return (0);
+}
 }
