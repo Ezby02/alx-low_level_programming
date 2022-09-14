d2680833bf224bf8e2fad7bd28643dae3c70dea9
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2022-09-15 00:26:56 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,12 +11,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		_putchar(43 + 0)
+	{
+		_putchar(43 + 0);
 		return (1);
+	}
 	else if (n == 0)
+	{
 		_putchar(0 + 48);
 		return (0);
+	}
 	else
+	{
 		_putchar(45 + 0);
 		return(-1);
+	}
 }
