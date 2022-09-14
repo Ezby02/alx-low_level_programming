151de486e57320be55dfe9e677dddef12aab4031
Ezby02 <esperancemwanza#gmail.com>
2022-09-14 12:07:00 -0700

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main-entry point
  *
  * Print alphabet in lowercase
- *
- * Return: Always 0
  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-int print_alphabet(void)
+void print_alphabet(void)
 {
 	int letter;
 	
@@ -20,5 +11,4 @@ int print_alphabet(void)
 		putchar(letter);
 	
 	putchar("\n");
-	return (void);
 }
