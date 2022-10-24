48564c00a8adeaf8412b99062e34eb1f3bd8de26
Ezby02 <111150589+Ezby02@users.noreply.github.com>
2022-10-24 19:28:01 +0300

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
-#include "list.h"
+#include "lists.h"
 /**
- * list_len - find number of elements in linked list
- * @h: pointer to start of linked list
+ * list_len - finds the number of elements in a linked list
+ * @h: pointer to the start of the linked list
  *
- * Return: number of elements in linked list
+ * Return: number of elements in the linked list
  */
 size_t list_len(const list_t *h)
 {
